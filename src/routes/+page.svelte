4bc869dd260d8fa0cee412757199149d7c8f7bc4
Magnus Karström <mrkarstrom81@gmail.com>
2024-10-19 20:47:22 +0200

    wip (Header.svelte): Declaring more than one variable in ts.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script>
 	import Header from './Header.svelte';
-
+	let text = 'This is so amazing!';
 	let name = 'Magnus';
 </script>
 
-<Header {name}></Header>
-
+<Header {name} {text}></Header>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	let {
-		name
+		name,
+		text
 	}: {
 		name: string;
+		text: string;
 	} = $props();
 </script>
 
 <h1>Hello {name}</h1>
 <h1>Your secret name is {name.replace('u', '*')}</h1>
+<h1>Your special name is {text.replaceAll('a', 'X')}</h1>
 
 <style>
 </style>
