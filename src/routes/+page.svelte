84698df4eb46f48d62e0280c449b73fddc87c70a
Magnus Karström <mrkarstrom81@gmail.com>
2024-10-19 21:32:59 +0200

    wip (+page.svelte): Binding the toggle onclick function tot state.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,19 @@
-<script>
+<script lang="ts">
 	import Header from './Header.svelte';
 	let text = $state('This is so amazing!');
 	let name = $state('Magnus');
+
+	let status: 'OPEN' | 'CLOSED' = $state('OPEN');
+
+	function toggle() {
+		status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
+	}
 </script>
 
 <Header {name} {text}></Header>
 
 <input type="text" bind:value={name} />
 <input type="text" bind:value={text} />
+
+<p>The store is now {status}</p>
+<button onclick={toggle}>Toggle Status</button>
