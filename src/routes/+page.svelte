e63c0d1e4f210123b674207a068482e84b045783
Magnus Karström <mrkarstrom81@gmail.com>
2024-10-23 20:48:32 +0200

    init (+page.svelte): Making a new formState function.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,21 @@
 <script lang="ts">
+	import { error } from '@sveltejs/kit';
 	import Header from './Header.svelte';
-	let text = $state('This is so amazing!');
-	let name = $state('Magnus');
-	let fullName = $derived(name + ' ' + 'Karström');
-	let status: 'OPEN' | 'CLOSED' = $state('OPEN');
-	let fake_name = 'Wes';
 
-	function toggle() {
-		status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
-	}
+	let formState = $state({
+		name: '',
+		birthday: '',
+		step: 0,
+		error: ''
+	});
 </script>
 
-<div class="w-full min-h-[100vh] flex flex-col items-center justify-center gap-4">
-	<Header {name} {text}></Header>
-
-	<h2>Hello {fullName}</h2>
-	<input type="text" bind:value={name} />
-	<input type="text" bind:value={text} />
-
-	<p>The store is now {status}</p>
-	<button
-		class="btn button bg-gray-400 border p-3 rounded-md"
-		onclick={() => (status = status === 'OPEN' ? 'CLOSED' : 'OPEN')}>Toggle Status</button
-	>
-
-	<button class="btn button bg-gray-400 border p-3 rounded-md" id="btn2" onclick={toggle}
-		>Button2</button
-	>
-	<h2>Hey {fake_name}</h2>
-</div>
+<main>
+	<p>Step: {formState.step}</p>
+	{#if formState.step === 0}
+		<div class="">
+			<label for="name">Your name</label>
+			<input type="text" id="name" bind:value={formState.name} />
+		</div>
+	{/if}
+</main>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,20 +1,5 @@
 <script lang="ts">
-	let {
-		name,
-		text,
-		fake_name = null
-	}: {
-		name: string;
-		text: string;
-		fake_name?: null | string;
-	} = $props();
 </script>
-
-<h1>Hello {name}</h1>
-<h1>Your secret name is {name.replace('u', '*')}</h1>
-<h1>Your special name is {text.replaceAll(/[a-z]/gi, '*')}</h1>
-
-<p>{fake_name}</p>
 
 <style>
 </style>
