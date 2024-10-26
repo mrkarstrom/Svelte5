71ea6603f6a1b83b88340991f0daebbb255c6eed
Magnus Karström <mrkarstrom81@gmail.com>
2024-10-26 10:05:59 +0200

    wip (+page.svelte): Working with the #each.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,33 +8,64 @@
 		step: 0,
 		error: ''
 	});
+
+	const QUESTIONS = [
+		{
+			question: 'Vad heter du?',
+			id: 'name',
+			type: 'text'
+		}
+	];
+
+	
 </script>
 
-<Header name={formState.name}>
-	<p>Hello</p>
-	{#snippet secondChild(name)}
-		<p>Second Child is: {name}</p>
-	{/snippet}
-</Header>
+<Header name={formState.name} />
 
 <main class="w-screen min-h-screen flex flex-col items-center justify-center gap-2">
 	<p>Steg: {formState.step + 1}</p>
 
-	{@render formStep({ question: 'Vad heter du?', id: 'name', type: 'text' })}
+	{#each QUESTIONS as question (question.id)}
+		{question.id}
+		{@render formStep({ question: 'Vad heter du?', id: 'name', type: 'text' })}
+	{/each}
 
-	{#if formState.error}
-		<p class="error">{formState.error}</p>
+	{#if formState.step === 0}
+		<div>
+			<label for="name">Ditt namn</label>
+			<input type="text" id="name" bind:value={formState.name} />
+		</div>
+		<button
+			class="btn w-[300px] p-3 bg-green-300 border-4 border-green-400 rounded-md hover:bg-transparent"
+			onclick={() => {
+				if (formState.name !== '') {
+					formState.step += 1;
+					formState.error = '';
+				} else {
+					formState.error = 'Skriv in ditt namn!';
+					alert(formState.error);
+				}
+			}}>Next</button
+		>
+	{:else if formState.step === 1}
+		<div class="">
+			<label for="bday">Din födelsedag</label>
+			<input type="text" id="bday" bind:value={formState.birthday} />
+		</div>
+		<button
+			class="btn w-[300px] p-3 bg-green-300 border-4 border-green-400 rounded-md hover:bg-transparent"
+			onclick={() => {
+				if (formState.birthday !== '') {
+					formState.step += 1;
+					formState.error = '';
+				} else {
+					formState.error = 'Skriv in din födelsedag!';
+					alert(formState.error);
+				}
+			}}>Next</button
+		>
 	{/if}
 </main>
-
-{#snippet formStep({ question, id, type }: { type: string; id: string; question: string })}
-	<article>
-		<div>
-			<label for={id}>{question}</label>
-			<input {type} {id} bind:value={formState[id]} />
-		</div>
-	</article>
-{/snippet}
 
 <style>
 	.error {
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,27 +1,13 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
-
 	let {
-		name,
-		children,
-		secondChild
+		name
 	}: {
 		name: string;
-		children: Snippet;
-		secondChild: Snippet;
 	} = $props();
 </script>
 
 <div>
-	{#if name}
-		<h1>{name}'s form</h1>
-	{/if}
-
 	<h2>{name ? name : 'User'}'s form too!'</h2>
-
-	{@render children()}
-
-	<h3>{@render secondChild('Magnus')}</h3>
 </div>
 
 <style>
